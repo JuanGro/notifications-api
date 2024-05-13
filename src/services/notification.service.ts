46398073605aa1e47e3132a4e657ef255c9f7d6a
JuanGro <ing.juangro@gmail.com>
2024-05-13 14:34:19 -0600

    Refactor

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -1,63 +1,76 @@
 import database from "../config/database.connection";
 import Category from "../models/category.model";
+import Channel from "../models/channel.model";
 import Email from "../models/email.model";
+import Notification from "../models/notification.model";
 import PushNotification from "../models/push-notification.model";
 import Sms from "../models/sms.model";
+import User from "../models/user.model";
 import { getUsersByCategorySubscribed } from "./user.service";
 
 export const sendNotifications = async (
   category: Category,
   message: string
 ) => {
-  const users = await getUsersByCategorySubscribed(category.name);
-  const now = new Date();
   const notifications: (Email | Sms | PushNotification)[] = [];
+  const users = await getUsersByCategorySubscribed(category.name);
+  const time = new Date();
   users.map((user) => {
-    user.channels.map(async (channel) => {
-      switch (channel.name) {
-        case "E-Mail":
-          const email = new Email(
-            message,
-            now,
-            category,
-            channel,
-            user,
-            "sender@gmail.com",
-            user.email,
-            "Notification",
-            message
-          );
-          notifications.push(email);
-          break;
-        case "SMS":
-          const sms = new Sms(
-            message,
-            now,
-            category,
-            channel,
-            user,
-            "323",
-            user.phoneNumber
-          );
-          notifications.push(sms);
-          break;
-        case "Push Notification":
-          const pushNotification = new PushNotification(
-            message,
-            now,
-            category,
-            channel,
-            user,
-            user.deviceId
-          );
-          notifications.push(pushNotification);
-          break;
-      }
+    user.channels.map((channel) => {
+      let notification: Notification = {
+        message,
+        time,
+        category,
+        channel,
+        user,
+      };
+      const newNotification = createNotification(channel, notification, user);
+      notifications.push(newNotification);
     });
   });
   await saveNotifications(notifications);
 };
 
-export const saveNotifications = async (notifications: (Email | Sms | PushNotification)[]) => {
+export const createNotification = (
+  channel: Channel,
+  notification: Notification,
+  user: User
+) => {
+  let newNotification;
+  switch (channel.name) {
+    case "E-Mail":
+      const email: Email = {
+        ...notification,
+        sender: "sender@gmail.com",
+        receiver: user.email,
+        subject: "Notification",
+        body: notification.message,
+      };
+      newNotification = email;
+      break;
+    case "SMS":
+      const sms: Sms = {
+        ...notification,
+        sender: "323",
+        receiver: user.phoneNumber,
+      };
+      newNotification = sms;
+      break;
+    case "Push Notification":
+      const pushNotification: PushNotification = {
+        ...notification,
+        deviceId: user.deviceId,
+      };
+      newNotification = pushNotification;
+      break;
+    default:
+      throw new Error("Notification type not recognized!");
+  }
+  return newNotification;
+};
+
+export const saveNotifications = async (
+  notifications: (Email | Sms | PushNotification)[]
+) => {
   await database.collection("notifications").insertMany(notifications);
 };
